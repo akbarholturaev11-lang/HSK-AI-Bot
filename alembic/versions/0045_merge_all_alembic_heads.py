a53223e3bbc401ebe753b5a7ad25335c5d81fae4
Akbarshoh <kaijimima1234@MacBook-Air.local>
2026-06-20 19:36:37 +0800

    Merge all Alembic heads

---
+++ alembic/versions/0045_merge_all_alembic_heads.py
@@ -0,0 +1,23 @@
+"""Merge all Alembic heads
+
+Revision ID: 0045_merge_all_alembic_heads
+Revises: 0044_add_daily_practice_fields, 0044_merge_payment_qr_and_release_feedback_heads
+Create Date: 2026-06-20
+
+"""
+
+revision = "0045_merge_all_alembic_heads"
+down_revision = (
+    "0044_add_daily_practice_fields",
+    "0044_merge_payment_qr_and_release_feedback_heads",
+)
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
