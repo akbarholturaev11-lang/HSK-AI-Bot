c66a50407a3fc49a8b8238b2525fb1fbda9f7091
Akbarshoh <kaijimima1234@MacBook-Air.local>
2026-06-20 22:36:13 +0800

    fix: merge course pilot alembic head

--- alembic/versions/0045_merge_all_alembic_heads.py
+++ alembic/versions/0045_merge_all_alembic_heads.py
@@ -1,13 +1,14 @@
 """Merge all Alembic heads
 
 Revision ID: 0045_merge_all_alembic_heads
-Revises: 0044_add_daily_practice_fields, 0044_merge_payment_qr_and_release_feedback_heads
+Revises: 0043_add_course_pilot_events, 0044_add_daily_practice_fields, 0044_merge_payment_qr_and_release_feedback_heads
 Create Date: 2026-06-20
 
 """
 
 revision = "0045_merge_all_alembic_heads"
 down_revision = (
+    "0043_add_course_pilot_events",
     "0044_add_daily_practice_fields",
     "0044_merge_payment_qr_and_release_feedback_heads",
 )
