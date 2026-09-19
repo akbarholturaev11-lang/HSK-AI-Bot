1e9342663e665d0be570af6316a60b16b771c63f
akbarshox <akbarholturaev11@gmail.com>
2026-09-19 16:41:45 +0800

    chore(android-ui): clean lesson imports

--- android/app/src/main/java/com/pomp/hskai/feature/lesson/LessonScreen.kt
+++ android/app/src/main/java/com/pomp/hskai/feature/lesson/LessonScreen.kt
@@ -4,7 +4,6 @@ import com.pomp.hskai.core.network.ApiError
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
