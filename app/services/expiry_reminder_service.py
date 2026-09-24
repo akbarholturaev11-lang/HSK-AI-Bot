3095a97f5b846484fbf3d9583fa48e181904137d
akbarshox <akbarholturaev11@gmail.com>
2026-09-24 10:08:24 +0800

    Reconcile Telegram reachability on expiry reminder

--- app/services/expiry_reminder_service.py
+++ app/services/expiry_reminder_service.py
@@ -32,6 +32,7 @@ class ExpiryReminderService:
                     chat_id=user.telegram_id,
                     text=text,
                 )
+                await block_service.handle_send_success(user)
                 await CourseNotificationService(self.session).record_from_text(
                     user,
                     key="subscription_expiring",
