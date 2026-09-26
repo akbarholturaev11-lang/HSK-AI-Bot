0b35f87916acebcc2f294b6e4fb555af576ee2ef
akbarshox <akbarholturaev11@gmail.com>
2026-09-27 00:26:39 +0800

    android: accept checkout origin in direct limit gate

--- android/app/src/direct/java/com/pomp/hskai/feature/limit/LimitGateHost.kt
+++ android/app/src/direct/java/com/pomp/hskai/feature/limit/LimitGateHost.kt
@@ -21,7 +21,7 @@ fun rememberLimitGate(
     supportUrl: String,
     isRefreshing: Boolean,
     onRefreshAccess: () -> Unit,
-    onOpenSubscription: () -> Unit = {},
+    onOpenSubscription: (String) -> Unit = {},
     trialEligible: Boolean = false,
     trialStarting: Boolean = false,
     trialError: String = "",
